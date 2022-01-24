dd7bcb9e2c2daa99fff1514111d85d324bd2f75f
Thomas <venturini.thomas95@gmail.com>
2022-01-24 17:03:36 +0100

    .

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 let boxNumber = parseInt(prompt("Quanti elementi vuoi visualizzare?"));
 
 if (isNaN(boxNumber)) {
-    alert("Il valore inserito non è valido, inserire un numero!");
+    alert("Il valore inserito non è valido, ricarica la pagina e inserisci un numero!");
 } else {
     for (i = 1; i <=boxNumber; i++) {
-        document.getElementById("container").innerHTML +=  `<div id="box">` + i + `</div>`;
-    
-    }
+        // document.getElementById("container").innerHTML +=  `<div id="box">` + i + `</div>`;
 
+        //if per decidere il contenuto dei box
+
+        if ( i % 15 == 0) {
+            document.getElementById("container").innerHTML += `<div id="box" class="fizz-buzz"> FizzBuzz </div>`;
+        } else if ( i % 3 == 0) {
+            document.getElementById("container").innerHTML += `<div id="box" class="fizz"> Fizz </div>`;
+        } else if ( i % 5 == 0) {
+            document.getElementById("container").innerHTML += `<div id="box" class="buzz"> Buzz </div>`;
+        } else {
+            document.getElementById("container").innerHTML += `<div id="box" class="default">` + i +  `</div>`;
+        }
+    }
     }
 
